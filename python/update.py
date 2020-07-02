a4c671bfc82c2b490e0aa240ff0694fb460cad58
fhlt2008 <fhlt2008@qq.com>
2020-07-02 14:04:53 +0800

    删除隐私数据

--- python/update.py
+++ python/update.py
@@ -7,14 +7,6 @@ url = "http://59.231.11.90/info/insertNetInfo"
 url1= "http://59.231.11.90/info/checkPhone"
 url2 = "http://59.231.11.90/info/selectNetInfo"
 url3 = "http://59.231.11.90/info/updateNetInfo"
-"""num = range(0,10)
-for i in num:
-    response = requests.post(url, headers=aheaders, data = json.dumps(adata[i]))
- 
-    print response.text
-{"str":"location_id='31311',c_name='大数据中心',contact='龙寒斯',phone='13467996789',join_up='裸光纤-50M'","phone":"13467996789"}
-
-"""
 f = open("idlist.json","r",encoding='utf-8')
 json_data = json.load(f)
 for i in range(20,len(json_data["data"])):
